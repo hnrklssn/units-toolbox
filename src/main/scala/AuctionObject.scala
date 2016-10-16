0daa9369e933bec34b25d67972ca0d25377ce883
hnrklssn <hnrklssn@gmail.com>
2016-10-16 19:47:35 +0200

    Added imagelink and estimated value to auction objects

--- src/main/scala/AuctionObject.scala
+++ src/main/scala/AuctionObject.scala
@@ -2,7 +2,7 @@ import org.joda.time.DateTime
 import java.io.Serializable
 
 @SerialVersionUID(314L)
-case class AuctionObject(id: Int, link: String, title: String, endtime: DateTime, bid: Int, bidder: String) extends Serializable {
+case class AuctionObject(id: Int, link: String, imageLink:String, title: String, endtime: DateTime, bid: Int, bidder: String, value: Int) extends Serializable {
 
   override def hashCode(): Int = id
   override def equals(other: Any) =
